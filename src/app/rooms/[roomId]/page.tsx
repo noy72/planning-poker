b7ddf259b3ee9010cb2416c0eb72ee4a24e6f31c
pokutuna <mail@pokutuna.com>
2026-09-04 18:36:19 +0900

    fix: remove unused notFound import

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound, redirect } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 import { RoomClient } from '@/components/RoomClient'
 import { joinRoom } from '@/lib/actions'
